ed31c7585dd6648bd13ed855996aff60ca3a1968
UsuarioEnGit <micaelapicasso.0610@gmail.com>
2020-04-29 20:30:54 -0300

    Agrego atributos en clase Hechizo y Poder.

--- JuegoHarryPotter/src/app/poderes/Poder.java
+++ JuegoHarryPotter/src/app/poderes/Poder.java
@@ -2,4 +2,23 @@ package app.poderes;
 
 public class Poder {
 
+    private String nombre;
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
