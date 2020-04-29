6ae48d2e6217645a4992a352fec9c930fd1baad5
UsuarioEnGit <micaelapicasso.0610@gmail.com>
2020-04-29 20:50:55 -0300

    Agrego atributos en clase Elfo y Personaje.

--- JuegoHarryPotter/src/app/personajes/Personaje.java
+++ JuegoHarryPotter/src/app/personajes/Personaje.java
@@ -2,4 +2,33 @@ package app.personajes;
 
 public class Personaje {
 
+    private String nombre;
+    private int salud;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+
 }
