8832a48488795ed4d74461bfa0c5908cddaf891f
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-04-29 20:43:33 -0300

    Atributos de la clase Artefacto creados. Getters y Setters de cada atributo creados.

--- JuegoHarryPotter/src/app/artefactos/Artefacto.java
+++ JuegoHarryPotter/src/app/artefactos/Artefacto.java
@@ -1,5 +1,44 @@
 package app.artefactos;
 
+import app.poderes.Poder;
+
 public class Artefacto {
+
+    private String nombre;
+    private double amplificadorDeDanio;
+    private double amplificadorDeCuracion;
+    private Poder poder;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getAmplificadorDeDanio() {
+        return amplificadorDeDanio;
+    }
+
+    public void setAmplificadorDeDanio(double amplificadorDeDanio) {
+        this.amplificadorDeDanio = amplificadorDeDanio;
+    }
+
+    public double getAmplificadorDeCuracion() {
+        return amplificadorDeCuracion;
+    }
+
+    public void setAmplificadorDeCuracion(double amplificadorDeCuracion) {
+        this.amplificadorDeCuracion = amplificadorDeCuracion;
+    }
+
+    public Poder getPoder() {
+        return poder;
+    }
+
+    public void setPoder(Poder poder) {
+        this.poder = poder;
+    }
 
 }
