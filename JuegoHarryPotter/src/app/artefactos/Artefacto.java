aae7abeaefd3e51774f4169ebc0c6ac67aab2981
UsuarioEnGit <micaelapicasso.0610@gmail.com>
2020-04-29 21:19:35 -0300

    La clase implementa la interfaz IEsMagico y se agregan los métodos de la interfaz

--- JuegoHarryPotter/src/app/artefactos/Artefacto.java
+++ JuegoHarryPotter/src/app/artefactos/Artefacto.java
@@ -1,5 +1,19 @@
 package app.artefactos;
 
-public class Artefacto {
+import app.IEsMagico;
+
+public class Artefacto implements IEsMagico {
+
+    @Override
+    public boolean esInvisibleAMuggles() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean esInvisible() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
 }
