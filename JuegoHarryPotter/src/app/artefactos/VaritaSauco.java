d00ce11498425d80627ee7d9c76b0afd47bf6ef7
UsuarioEnGit <micaelapicasso.0610@gmail.com>
2020-04-29 21:20:46 -0300

    Las clases implementan las interfaz IReliquiasMuerte y se agregan los métodos de la interfaz.

--- JuegoHarryPotter/src/app/artefactos/VaritaSauco.java
+++ JuegoHarryPotter/src/app/artefactos/VaritaSauco.java
@@ -1,5 +1,13 @@
 package app.artefactos;
 
-public class VaritaSauco extends Varita{
+import app.IReliquiaMuerte;
+
+public class VaritaSauco extends Varita implements IReliquiaMuerte{
+
+    @Override
+    public boolean esReliquiaMuerte() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
 }
