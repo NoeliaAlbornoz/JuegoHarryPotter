55dc3da0dc8cab9b9a242ef9ff0beb210d677343
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-04-29 21:11:55 -0300

    Clase Elfo implementa Interfaz IHaceMagia. Se agregaron los métodos de la Interfaz.

--- JuegoHarryPotter/src/app/personajes/Elfo.java
+++ JuegoHarryPotter/src/app/personajes/Elfo.java
@@ -2,10 +2,14 @@ package app.personajes;
 
 import java.util.List;
 
+import app.IEsMagico;
+import app.IHaceMagia;
 import app.artefactos.Artefacto;
 import app.poderes.Hechizo;
+import app.poderes.Poder;
 
-public class Elfo extends Criatura {
+public class Elfo extends Criatura implements IHaceMagia {
+
     private int energiaMagica;
     private Artefacto artefacto;
     private List<Hechizo> hechizos;
@@ -32,6 +36,36 @@ public class Elfo extends Criatura {
 
     public void setHechizos(List<Hechizo> hechizos) {
         this.hechizos = hechizos;
+    }
+
+    @Override
+    public Poder getPoderInicial() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setPoder(Poder poder) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void aprender(Hechizo h) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void atacar(Personaje personaje, Hechizo hechizo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void atacar(Personaje personaje, String hechizo) {
+        // TODO Auto-generated method stub
+
     }
 
 }
