12011597be10b860000493b0e0a326bec3136e3e
UsuarioEnGit <micaelapicasso.0610@gmail.com>
2020-05-01 15:35:57 -0300

    Se crearon los atributos nombre y descripcion, getters y setters.

--- JuegoHarryPotter/src/app/transportes/Transporte.java
+++ JuegoHarryPotter/src/app/transportes/Transporte.java
@@ -2,4 +2,23 @@ package app.transportes;
 
 public class Transporte {
 
+    private String nombre;
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
