c3ba1bf56076e6029fcb4c2acfc80aa91a412c8d
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-05-01 15:49:20 -0300

    Agrego constructor (super()).

--- JuegoHarryPotter/src/app/poderes/Cavelnimicum.java
+++ JuegoHarryPotter/src/app/poderes/Cavelnimicum.java
@@ -2,4 +2,9 @@ package app.poderes;
 
 public class Cavelnimicum extends HechizoDefenza {
 
+    public Cavelnimicum(String nombre) {
+        super(nombre);
+        // TODO Auto-generated constructor stub
+    }
+
 }
