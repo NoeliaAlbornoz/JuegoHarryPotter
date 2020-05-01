bd691e94563e8b47edc3090a23daf5c51d1631c4
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-05-01 15:03:58 -0300

    Introducción de banners bienvenida y despedida del juego. Creación de objeto Clase JuegoHP.

--- JuegoHarryPotter/src/app/App.java
+++ JuegoHarryPotter/src/app/App.java
@@ -3,5 +3,54 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        JuegoHP juego = new JuegoHP();
+
+        bannerLogoBienvenida();
+
+        bannerDespedida();
+
     }
+
+    public static void bannerLogoBienvenida() {
+
+        System.out.println();
+        System.out.println("***************************************************************************************************************");
+        System.out.println("*                                                                                                             *");
+        System.out.println("*                                                     ******    **                                            *");
+        System.out.println("*                                                      ***** *    ***                                         *");
+        System.out.println("*  *****     *****                                      ****      ***                                         *");
+        System.out.println("*    ***     ***                    *****        *      ****      ***                  *                      *");
+        System.out.println("*    ***     ***                    *      *    *       ****      ***          *       *                      *");
+        System.out.println("*    ***     ***    *****   *****   *       *  *         ***  *   **           *     *****      * *           *");
+        System.out.println("*    ***********        *   *       *        *       *******   *             *****     *       *   *          *");
+        System.out.println("*    ***     ***    *****   *            *   *          ****         *  *      *       *       *****          *");
+        System.out.println("*    ***     ***    *   *   *              *              **        *    *     *       *       *       *****  *");
+        System.out.println("*    ***     ***    *****                              *****        *    *     *       * *      * *    *      *");
+        System.out.println("*  *****     *****                                       ***        *    *     *       * *             *      *");
+        System.out.println("*                                                         **         *  *      * *      *              *      *");
+        System.out.println("*                                                          *                   * *                            *");
+        System.out.println("*                                                                                                             *");
+        System.out.println("***************************************************************************************************************");
+        System.out.println("\n                                             ¡¡¡BIENVENIDOS!!!                                             \n");
+    
+    }
+    
+    
+    public static void bannerDespedida() {
+    
+        System.out.println();
+        System.out.println("**************************************************************************************");
+        System.out.println();
+        System.out.println(" *    *   ***   ****   ****   *    *     *****   *****   *****   ***** *****   ****   ");
+        System.out.println(" *    *  *   *  *   *  *   *   *  *      *    *  *   *     *       *   *       *   *  ");
+        System.out.println(" ******  *****  ****   ****     *        *****   *   *     *       *   ****    ****   ");
+        System.out.println(" *    *  *   *  *   *  *   *    *        *       *   *     *       *   *       *   *  ");
+        System.out.println(" *    *  *   *  *    * *    *   *        *       *****     *       *   *****   *    * ");
+        System.out.println();
+        System.out.println("**************************************************************************************");
+        System.out.println();
+    }
+    
 }
+
+
