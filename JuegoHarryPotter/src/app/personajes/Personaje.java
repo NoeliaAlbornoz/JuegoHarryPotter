779f2f4fb07f6da8f8bf6a1521227838149d6db4
UsuarioEnGit <micaelapicasso.0610@gmail.com>
2020-05-03 18:07:47 -0300

    Creación del método tirarDado():

--- JuegoHarryPotter/src/app/personajes/Personaje.java
+++ JuegoHarryPotter/src/app/personajes/Personaje.java
@@ -1,6 +1,11 @@
 package app.personajes;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public abstract class Personaje {
+
+    public static Scanner Teclado = new Scanner(System.in);
 
     private String nombre;
     private int salud;
@@ -30,15 +35,26 @@ public abstract class Personaje {
         this.edad = edad;
     }
 
-    public  boolean estaVivo(){
-        return salud > 0 ;
+    public boolean estaVivo() {
+        return salud > 0;
     }
 
-    public void disminuirSalud(int danio){
+    public void disminuirSalud(int danio) {
 
         int saludRestante = this.getSalud() - danio;
 
         this.setSalud(saludRestante);
+
+    }
+
+    public int tirarDado() {
+        System.out.print("Dado mágico: ");
+
+        Random rand = new Random(System.nanoTime());
+
+        Teclado.nextLine();
+
+        return rand.nextInt(10);
 
     }
 
