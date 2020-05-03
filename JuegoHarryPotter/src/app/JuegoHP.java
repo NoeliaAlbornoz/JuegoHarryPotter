49e3294374ac39b3fafb136a1e7ba3eeb8bd1b5f
UsuarioEnGit <micaelapicasso.0610@gmail.com>
2020-05-03 18:43:59 -0300

    Se crean objetos wizard.

--- JuegoHarryPotter/src/app/JuegoHP.java
+++ JuegoHarryPotter/src/app/JuegoHP.java
@@ -222,6 +222,60 @@ public class JuegoHP {
 
         this.personajes.add(wizard);
 
+        wizard = new Wizard();
+        wizard.setNombre("Draco Malfoy");
+        wizard.setEnergiaMagica(150);
+        wizard.setSalud(100);
+        wizard.setEdad(17);
+        wizard.setMagoOscuro(false);
+
+        poder = new Poder("Metamorfosis");
+        poder.setDescripcion("La metamorfosis es un poder usado para la transformación de un animal en otro");
+        wizard.setPoder(poder);
+
+        escoba = new Escoba();
+        escoba.setNombre(" Nimbus 2000 ");
+        escoba.setDescripcion("La Nimbus 2000 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
+        wizard.setEscoba(escoba);
+
+        this.personajes.add(wizard);
+
+        wizard = new Wizard();
+        wizard.setNombre("Luna Lovegood");
+        wizard.setEnergiaMagica(150);
+        wizard.setSalud(100);
+        wizard.setEdad(17);
+        wizard.setMagoOscuro(false);
+
+        poder = new Poder("Metamorfosis");
+        poder.setDescripcion("La metamorfosis es un poder usado para la transformación de un animal en otro");
+        wizard.setPoder(poder);
+
+        escoba = new Escoba();
+        escoba.setNombre(" Nimbus 2000 ");
+        escoba.setDescripcion("La Nimbus 2000 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
+        wizard.setEscoba(escoba);
+
+        this.personajes.add(wizard);
+
+        wizard = new Wizard();
+        wizard.setNombre("Nevile Longbottom");
+        wizard.setEnergiaMagica(150);
+        wizard.setSalud(100);
+        wizard.setEdad(17);
+        wizard.setMagoOscuro(false);
+
+        poder = new Poder("Metamorfosis");
+        poder.setDescripcion("La metamorfosis es un poder usado para la transformación de un animal en otro");
+        wizard.setPoder(poder);
+
+        escoba = new Escoba();
+        escoba.setNombre(" Nimbus 2000 ");
+        escoba.setDescripcion("La Nimbus 2000 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
+        wizard.setEscoba(escoba);
+
+        this.personajes.add(wizard);
+
     }
 
     public void inicializarHechizos() {
