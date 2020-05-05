86471ac4a5b08bc771c46d22d59d6dc1c325ac7d
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-05-05 17:26:25 -0300

    Se completaron bodies de métodos esInvisible y esInvisibleAMuggles.

--- JuegoHarryPotter/src/app/personajes/Criatura.java
+++ JuegoHarryPotter/src/app/personajes/Criatura.java
@@ -6,14 +6,13 @@ public abstract class Criatura extends Personaje implements IEsMagico{
 
     @Override
     public boolean esInvisibleAMuggles() {
-       
-        return false;
+        return getEdad()  >= 2 && getEdad() < 5;
     }
 
     @Override
     public boolean esInvisible() {
        
-        return false;
+        return getEdad() >= 5 && getEdad() <= 10;
     }
 
 }
