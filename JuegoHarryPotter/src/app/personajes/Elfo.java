8fe41a793bd34b8684906b359cd70b5274506d64
UsuarioEnGit <micaelapicasso.0610@gmail.com>
2020-05-05 19:03:48 -0300

    Se creo el atributo poderInicial y se hicieron getter y setter, por lo tanto se eliminó @Override Poder getPoderInicial(). Format

--- JuegoHarryPotter/src/app/personajes/Elfo.java
+++ JuegoHarryPotter/src/app/personajes/Elfo.java
@@ -13,6 +13,7 @@ public class Elfo extends Criatura implements IHaceMagia {
     private int energiaMagica;
     private Artefacto artefacto;
     private List<Hechizo> hechizos = new ArrayList<>();
+    private Poder poderInicial;
 
     public int getEnergiaMagica() {
         return energiaMagica;
@@ -39,14 +40,7 @@ public class Elfo extends Criatura implements IHaceMagia {
     }
 
     @Override
-    public Poder getPoderInicial() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
     public void setPoder(Poder poder) {
-        // TODO Auto-generated method stub
 
     }
 
@@ -63,8 +57,10 @@ public class Elfo extends Criatura implements IHaceMagia {
 
     public void aumentarEnergiaMagicaElfo() {
 
-        System.out.println("Puedes saber la edad de tu elfo al tirar el dado mágico. Si su edad es mayor o igual a 2 pero menor a 5, ganas 1 punto de energía mágica.\n");
-        System.out.println("Si su edad es mayor o igual a 5 pero menor o igual a 10, ganas 3 puntos de energía mágica.\n");
+        System.out.println(
+                "Puedes saber la edad de tu elfo al tirar el dado mágico. Si su edad es mayor o igual a 2 pero menor a 5, ganas 1 punto de energía mágica.\n");
+        System.out.println(
+                "Si su edad es mayor o igual a 5 pero menor o igual a 10, ganas 3 puntos de energía mágica.\n");
 
         this.setEdad(this.tirarDado());
 
@@ -72,13 +68,15 @@ public class Elfo extends Criatura implements IHaceMagia {
 
             this.energiaMagica++;
 
-            System.out.println("Tu elfo tiene " + this.getEdad() + " años. Su energía mágica es de  " + this.getEnergiaMagica() + " puntos.");
+            System.out.println("Tu elfo tiene " + this.getEdad() + " años. Su energía mágica es de  "
+                    + this.getEnergiaMagica() + " puntos.");
 
         } else if (this.esInvisible()) {
 
             this.energiaMagica += 3;
 
-            System.out.println("Tu elfo tiene " + this.getEdad() + " años. Su energía mágica es de  " + this.getEnergiaMagica() + " puntos.");
+            System.out.println("Tu elfo tiene " + this.getEdad() + " años. Su energía mágica es de  "
+                    + this.getEnergiaMagica() + " puntos.");
 
         } else {
 
@@ -89,8 +87,6 @@ public class Elfo extends Criatura implements IHaceMagia {
 
     @Override
     public void atacar(Personaje personaje, Hechizo hechizo) {
-
-        
 
         this.disminuirEnergiaMagica(energiaMagica, hechizo);
 
@@ -134,5 +130,12 @@ public class Elfo extends Criatura implements IHaceMagia {
 
     }
 
+    public Poder getPoderInicial() {
+        return poderInicial;
+    }
+
+    public void setPoderInicial(Poder poderInicial) {
+        this.poderInicial = poderInicial;
+    }
 
 }
