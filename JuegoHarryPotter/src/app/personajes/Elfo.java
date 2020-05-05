aad5293fda490e46e9297f69d349f15d6a4304bb
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-05-05 00:30:28 -0300

    Métodos disminuirEnergiaMagica y aumentarSaludConCuracion creados. Se completó método atacar(Personaje personaje, String hechizo) y aprender(Hechizao h). Se completó y modificó método atacar(Personaje personaje, Hechizo hechizo).

--- JuegoHarryPotter/src/app/personajes/Elfo.java
+++ JuegoHarryPotter/src/app/personajes/Elfo.java
@@ -40,31 +40,67 @@ public class Elfo extends Criatura implements IHaceMagia {
 
     @Override
     public Poder getPoderInicial() {
-       
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public void setPoder(Poder poder) {
-   
+        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void aprender(Hechizo h) {
-      
+        
+        this.hechizos.add(h);
+
+        System.out.println("Has aprendido el hechizo " + h.getNombre());
 
     }
 
     @Override
     public void atacar(Personaje personaje, Hechizo hechizo) {
-  
+
+        this.disminuirEnergiaMagica(energiaMagica, hechizo);
+
+        personaje.disminuirSalud(hechizo, artefacto);
+
+        this.aumentarSaludConCuracion(hechizo, artefacto);
+
+        this.atacar(personaje, hechizo.getNombre());
+
+        System.out.println("Tu artefacto " + artefacto.getNombre() + " genera un daño adicional de "
+                + this.utilizarDanioDeArtefacto(hechizo, artefacto) + " puntos. ");
+
+        System.out.println("Tu artefacto " + artefacto.getNombre() + " genera una curación adicional de "
+                + this.utilizarCuracionDeArtefacto(hechizo, artefacto) + " puntos. ");
 
     }
 
     @Override
     public void atacar(Personaje personaje, String hechizo) {
-      
+
+        System.out.println("Has atacado con " + hechizo);
+
+        System.out.println("Te quedan " + this.energiaMagica + " puntos de energía mágica.");
+
+    }
+
+    public void aumentarSaludConCuracion(Hechizo hechizo, Artefacto artefacto) {
+
+        int saludAumentada = this.getSalud() + hechizo.getNivelCuracion()
+                + this.utilizarCuracionDeArtefacto(hechizo, artefacto);
+
+        this.setSalud(saludAumentada);
+
+    }
+
+    public void disminuirEnergiaMagica(int energiaMagica, Hechizo hechizo) {
+
+        int energiaMagicaResultante = this.energiaMagica - hechizo.getEnergiaMagica();
+
+        this.setEnergiaMagica(energiaMagicaResultante);
 
     }
 
