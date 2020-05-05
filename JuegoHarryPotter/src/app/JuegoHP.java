1cd920316e8dbb397e64c35f1410d29f2df00ae8
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-05-05 00:14:44 -0300

    Objetos Artefacto, Poder, Elfo creados. Instanciamiento de Hechizos particulares (reemplazaron superclase Hechizo - los hechizos base no se instancian). Imports.

--- JuegoHarryPotter/src/app/JuegoHP.java
+++ JuegoHarryPotter/src/app/JuegoHP.java
@@ -8,19 +8,20 @@ import app.artefactos.Artefacto;
 import app.personajes.Elfo;
 import app.personajes.Personaje;
 import app.personajes.Wizard;
+import app.poderes.Cavelnimicum;
 import app.poderes.Hechizo;
 import app.poderes.Poder;
+import app.poderes.SectumSempra;
+import app.poderes.VulneraSanentur;
+import app.poderes.WingwardumLeviosa;
 import app.transportes.Escoba;
 import app.transportes.TrenExpresoHowards;
 
 public class JuegoHP {
 
     public static Scanner Teclado = new Scanner(System.in);
-
     public List<Personaje> personajes = new ArrayList<>();
-
     public List<Hechizo> hechizos = new ArrayList<>();
-
     public TrenExpresoHowards tren;
 
     public void inicializarJuego() {
@@ -153,8 +154,8 @@ public class JuegoHP {
 
         Wizard wizard = new Wizard();
         wizard.setNombre("Harry Potter");
-        wizard.setEnergiaMagica(150);
-        wizard.setSalud(100);
+        wizard.setEnergiaMagica(130);
+        wizard.setSalud(90);
         wizard.setEdad(17);
         wizard.setMagoOscuro(false);
 
@@ -174,15 +175,14 @@ public class JuegoHP {
         artefacto.setAmplificadorDeDanio(0.5);
         artefacto.setAmplificadorDeCuracion(0.5);
         artefacto.setPoder(poder);
-
         wizard.setArtefacto(artefacto);
 
         this.personajes.add(wizard);
 
         wizard = new Wizard();
         wizard.setNombre("Ron Weasley");
-        wizard.setEnergiaMagica(150);
-        wizard.setSalud(100);
+        wizard.setEnergiaMagica(130);
+        wizard.setSalud(90);
         wizard.setEdad(17);
         wizard.setMagoOscuro(false);
 
@@ -202,20 +202,19 @@ public class JuegoHP {
         artefacto.setAmplificadorDeDanio(0.3);
         artefacto.setAmplificadorDeCuracion(0.5);
         artefacto.setPoder(poder);
-
         wizard.setArtefacto(artefacto);
 
         this.personajes.add(wizard);
 
         wizard = new Wizard();
         wizard.setNombre("Hermione Granger");
-        wizard.setEnergiaMagica(150);
-        wizard.setSalud(100);
+        wizard.setEnergiaMagica(130);
+        wizard.setSalud(90);
         wizard.setEdad(17);
         wizard.setMagoOscuro(false);
 
-        poderInicial = new Poder("Invisibilidad");
-        poderInicial.setDescripcion("La invisibilidad es un poder usado para desaparecer ante la vista de los demás");
+        poderInicial = new Poder("Metamorfosis");
+        poderInicial.setDescripcion("La metamorfosis es un poder usado para la transformación de un animal en otro");
         wizard.setPoder(poderInicial);
 
         escoba = new Escoba();
@@ -223,12 +222,21 @@ public class JuegoHP {
         escoba.setDescripcion("La Nimbus 2000 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
         wizard.setEscoba(escoba);
 
+        poder = new Poder("Invisibilidad");
+        poder.setDescripcion("La invisibilidad es un poder usado para desaparecer ante la vista de los demás");
+
+        artefacto = new Artefacto("Varita de Sauco");
+        artefacto.setAmplificadorDeDanio(0.3);
+        artefacto.setAmplificadorDeCuracion(0.5);
+        artefacto.setPoder(poder);
+        wizard.setArtefacto(artefacto);
+
         this.personajes.add(wizard);
 
         wizard = new Wizard();
         wizard.setNombre("Ginny Weasley");
-        wizard.setEnergiaMagica(150);
-        wizard.setSalud(100);
+        wizard.setEnergiaMagica(130);
+        wizard.setSalud(90);
         wizard.setEdad(16);
         wizard.setMagoOscuro(false);
 
@@ -241,14 +249,23 @@ public class JuegoHP {
         escoba.setDescripcion("La Nimbus 2000 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
         wizard.setEscoba(escoba);
 
+        poder = new Poder("Parse Tongue");
+        poder.setDescripcion("El Parse es la legua de las serpientes y de aquellos que pueden hablar con ellas");
+
+        artefacto = new Artefacto("Piedra de resurrección");
+        artefacto.setAmplificadorDeDanio(0.3);
+        artefacto.setAmplificadorDeCuracion(0.5);
+        artefacto.setPoder(poder);
+        wizard.setArtefacto(artefacto);
+
         this.personajes.add(wizard);
 
         wizard = new Wizard();
         wizard.setNombre("Draco Malfoy");
-        wizard.setEnergiaMagica(150);
-        wizard.setSalud(100);
+        wizard.setEnergiaMagica(130);
+        wizard.setSalud(90);
         wizard.setEdad(17);
-        wizard.setMagoOscuro(false);
+        wizard.setMagoOscuro(true);
 
         poderInicial = new Poder("Metamorfosis");
         poderInicial.setDescripcion("La metamorfosis es un poder usado para la transformación de un animal en otro");
@@ -258,13 +275,22 @@ public class JuegoHP {
         escoba.setNombre(" Nimbus 2000 ");
         escoba.setDescripcion("La Nimbus 2000 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
         wizard.setEscoba(escoba);
+
+        poder = new Poder("Parse Tongue");
+        poder.setDescripcion("El Parse es la legua de las serpientes y de aquellos que pueden hablar con ellas");
+
+        artefacto = new Artefacto("Horrocrux");
+        artefacto.setAmplificadorDeDanio(0.5);
+        artefacto.setAmplificadorDeCuracion(0.5);
+        artefacto.setPoder(poder);
+        wizard.setArtefacto(artefacto);
 
         this.personajes.add(wizard);
 
         wizard = new Wizard();
         wizard.setNombre("Luna Lovegood");
-        wizard.setEnergiaMagica(150);
-        wizard.setSalud(100);
+        wizard.setEnergiaMagica(130);
+        wizard.setSalud(90);
         wizard.setEdad(17);
         wizard.setMagoOscuro(false);
 
@@ -276,13 +302,22 @@ public class JuegoHP {
         escoba.setNombre(" Nimbus 2000 ");
         escoba.setDescripcion("La Nimbus 2000 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
         wizard.setEscoba(escoba);
+
+        poder = new Poder("Invisibilidad");
+        poder.setDescripcion("La invisibilidad es un poder usado para desaparecer ante la vista de los demás");
+
+        artefacto = new Artefacto("Capa de Invisibilidad");
+        artefacto.setAmplificadorDeDanio(0.5);
+        artefacto.setAmplificadorDeCuracion(0.5);
+        artefacto.setPoder(poder);
+        wizard.setArtefacto(artefacto);
 
         this.personajes.add(wizard);
 
         wizard = new Wizard();
-        wizard.setNombre("Nevile Longbottom");
-        wizard.setEnergiaMagica(150);
-        wizard.setSalud(100);
+        wizard.setNombre("Neville Longbottom");
+        wizard.setEnergiaMagica(130);
+        wizard.setSalud(90);
         wizard.setEdad(17);
         wizard.setMagoOscuro(false);
 
@@ -295,47 +330,98 @@ public class JuegoHP {
         escoba.setDescripcion("La Nimbus 2000 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
         wizard.setEscoba(escoba);
 
+        poder = new Poder("Parse Tongue");
+        poder.setDescripcion("El Parse es la legua de las serpientes y de aquellos que pueden hablar con ellas");
+
+        artefacto = new Artefacto("Horrocrux");
+        artefacto.setAmplificadorDeDanio(0.5);
+        artefacto.setAmplificadorDeCuracion(0.5);
+        artefacto.setPoder(poder);
+        wizard.setArtefacto(artefacto);
+
+        wizard = new Wizard();
+        wizard.setNombre("Profesora Sprout");
+        wizard.setEnergiaMagica(140);
+        wizard.setSalud(95);
+        wizard.setEdad(60);
+        wizard.setMagoOscuro(false);
+
+        poderInicial = new Poder("Metamorfosis");
+        poderInicial.setDescripcion("La metamorfosis es un poder usado para la transformación de un animal en otro");
+        wizard.setPoder(poderInicial);
+
+        escoba = new Escoba();
+        escoba.setNombre(" Nimbus 2001 ");
+        escoba.setDescripcion("La Nimbus 2001 se usa para jugar al Quidditch y es mas rápida que la escoba barredora");
+        wizard.setEscoba(escoba);
+
+        poder = new Poder("Invisibilidad");
+        poder.setDescripcion("La invisibilidad es un poder usado para desaparecer ante la vista de los demás");
+
+        artefacto = new Artefacto("Capa de invisibilidad");
+        artefacto.setAmplificadorDeDanio(0.5);
+        artefacto.setAmplificadorDeCuracion(0.5);
+        artefacto.setPoder(poder);
+        wizard.setArtefacto(artefacto);
+
         this.personajes.add(wizard);
+
+        Elfo elfo = new Elfo();
+        elfo.setNombre("Winky");
+        elfo.setEnergiaMagica(145);
+        elfo.setSalud(92);
+        elfo.setEdad(12);
+
+        poder = new Poder("Invisibilidad");
+        poder.setDescripcion("La invisibilidad es un poder usado para desaparecer ante la vista de los demás");
+
+        artefacto = new Artefacto("Varita de Sauco");
+        artefacto.setAmplificadorDeDanio(0.3);
+        artefacto.setAmplificadorDeCuracion(0.5);
+        artefacto.setPoder(poder);
+        elfo.setArtefacto(artefacto);
+        
+        this.personajes.add(elfo);
 
     }
 
     public void inicializarHechizos() {
 
-        Hechizo hechizo = new Hechizo("Wingwardum Leviosa");
-        hechizo.setDescripcion("Wingwardum Leviosa es un hechizo que permite levitar objetos o pequeños seres vivos.");
-        hechizo.setEnergiaMagica(2);
-        hechizo.setNivelDanio(5);
-        hechizo.setEsOscuro(false);
-        hechizo.setNivelCuracion(2);
+        WingwardumLeviosa wingwardumLeviosa = new WingwardumLeviosa("Wingwardum Leviosa");
+        wingwardumLeviosa.setDescripcion("Wingwardum Leviosa es un hechizo que permite levitar objetos o pequeños seres vivos.");
+        wingwardumLeviosa.setEnergiaMagica(2);
+        wingwardumLeviosa.setNivelDanio(5);
+        wingwardumLeviosa.setNivelCuracion(2);
+        wingwardumLeviosa.setEsOscuro(false);
     
-        this.hechizos.add(hechizo);
+        this.hechizos.add(wingwardumLeviosa);
 
-        hechizo = new Hechizo("Sectumsempra");
-        hechizo.setDescripcion("La maldición Sectumsempra genera cortes profundos en la víctima.");
-        hechizo.setEnergiaMagica(7);
-        hechizo.setNivelDanio(7);
-        hechizo.setEsOscuro(true);
-        hechizo.setNivelCuracion(1);
+        SectumSempra sectumsempra = new SectumSempra("Sectumsempra");
+        sectumsempra.setDescripcion("La maldición Sectumsempra genera cortes profundos en la víctima.");
+        sectumsempra.setEnergiaMagica(7);
+        sectumsempra.setNivelDanio(7);
+        sectumsempra.setNivelCuracion(1);
+        sectumsempra.setEsOscuro(true);
 
-        this.hechizos.add(hechizo);
+        this.hechizos.add(sectumsempra);
 
-        hechizo = new Hechizo("VulneraSanentur");
-        hechizo.setDescripcion("Hechizo sanador que corresponde al contrahechizo de la maldición sectumsempra.");
-        hechizo.setEnergiaMagica(4);
-        hechizo.setNivelDanio(2);
-        hechizo.setEsOscuro(false);
-        hechizo.setNivelCuracion(4);
+        VulneraSanentur vulnerasanentur = new VulneraSanentur("Vulnera Sanentur");
+        vulnerasanentur.setDescripcion("Hechizo sanador que corresponde al contrahechizo de la maldición sectumsempra.");
+        vulnerasanentur.setEnergiaMagica(4);
+        vulnerasanentur.setNivelDanio(2);
+        vulnerasanentur.setNivelCuracion(4);
+        vulnerasanentur.setEsOscuro(false);
 
-        this.hechizos.add(hechizo);
+        this.hechizos.add(vulnerasanentur);
 
-        hechizo = new Hechizo("Cavelnimicum");
-        hechizo.setDescripcion("Hechizo de protección, mantiene alejado a los enemigos.");
-        hechizo.setEnergiaMagica(3);
-        hechizo.setNivelDanio(4);
-        hechizo.setEsOscuro(false);
-        hechizo.setNivelCuracion(3);
+        Cavelnimicum  cavelnimicum = new Cavelnimicum("Cavelnimicum");
+        cavelnimicum.setDescripcion("Hechizo de protección, mantiene alejado a los enemigos.");
+        cavelnimicum.setEnergiaMagica(3);
+        cavelnimicum.setNivelDanio(4);
+        cavelnimicum.setNivelCuracion(3);
+        cavelnimicum.setEsOscuro(false);
 
-        this.hechizos.add(hechizo);
+        this.hechizos.add(cavelnimicum);
     }
 
     public Personaje seleccionarPersonaje() {
@@ -414,32 +500,7 @@ public class JuegoHP {
 
     }
 
-    public static void bannerAprenderHechizos() {
-
-        System.out.println("\n Debes aprender tus hechizos. ");
-        System.out.println("  ¿Cuál quieres entrenar?: \n");
-        System.out.println("Elegir hechizo: ");
-
-    }
-
-    public static void bannerAtacarConHechizos() {
-
-        System.out.println("\nDebes elegir un hechizo para atacar a tu oponente: \n");
-
-    }
-
-    public void bannerJugador1() {
-
-        System.out.println("JUGADOR 1 \n");
-
-    }
-
-    public void bannerJugador2() {
-
-        System.out.println("\nJUGADOR 2 \n");
-
-    }
-
+    
     public void iniciarAprendizajeDeHechizos(Personaje jugador) {
 
         Hechizo hechizo = this.seleccionarHechizo();
@@ -512,6 +573,32 @@ public class JuegoHP {
 
         this.iniciarMiniJuego(jugador2, tren);
         
+    }
+
+    public static void bannerAprenderHechizos() {
+
+        System.out.println("\n Debes aprender tus hechizos. ");
+        System.out.println("  ¿Cuál quieres entrenar?: \n");
+        System.out.println("Elegir hechizo: ");
+
+    }
+
+    public static void bannerAtacarConHechizos() {
+
+        System.out.println("\nDebes elegir un hechizo para atacar a tu oponente: \n");
+
+    }
+
+    public void bannerJugador1() {
+
+        System.out.println("JUGADOR 1 \n");
+
+    }
+
+    public void bannerJugador2() {
+
+        System.out.println("\nJUGADOR 2 \n");
+
     }
 
 }
