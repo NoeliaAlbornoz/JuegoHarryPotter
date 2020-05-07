da5c07a28d074c2302448cb74d40ae801ec39d80
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-05-07 09:35:40 -0300

    Minor fix.

--- JuegoHarryPotter/src/app/artefactos/Artefacto.java
+++ JuegoHarryPotter/src/app/artefactos/Artefacto.java
@@ -49,18 +49,14 @@ public class Artefacto implements IEsMagico {
 
     @Override
     public boolean esInvisibleAMuggles() {
-        
+       
         return false;
     }
 
     @Override
     public boolean esInvisible() {
-        
+       
         return false;
     }
-
-
-
-    
 
 }
